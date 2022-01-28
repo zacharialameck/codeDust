2a59d5b30d2d427ee35e48df939a1091c8cecaa2
zacharialameck <90456000+zacharialameck@users.noreply.github.com>
2022-01-28 12:26:17 +0000

    Add files via upload

---
+++ intro_p5js/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(20,500,400);
+  ellipse(100,100,200);
+  fill(100)
+  ellipseMode(CENTER);
+  ellipse(200,200,100);
+  fill(300)
+  triangle(100,100,300,10,20,10);
+  fill(200)
+    
+  
+}
