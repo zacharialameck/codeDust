1b49cc126329c950cc40d53c0dea22234031abf6
zacharialameck <90456000+zacharialameck@users.noreply.github.com>
2022-01-28 12:41:56 +0000

    Add files via upload

---
+++ colors_2022_01_28_12_38_02/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // when changing the numbers on background outomatically colors will change.
+  background(220,400,200,400);
+  //ellipse stand for the physical shape and those numbers reduced the shape of shape.
+  ellipse(200,200,100,100);
+  //the fill stand for changing the color inside the shape.
+  fill(100)
+  
+  
+  
+}
